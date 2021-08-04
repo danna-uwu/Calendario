4865eed6a51813fb318258079d559507502edd63
danna-uwu <66384664+danna-uwu@users.noreply.github.com>
2021-08-04 15:21:02 -0500

    Create proyecto.js

---
+++ proyecto.js
@@ -0,0 +1,101 @@
+importar  java  util . Escáner ;
+público class  meses {
+public  static  void  main ( String  arg [ ] ) {
+int  opcion ;
+Sistema . fuera . println  ( "meses" ) ;
+hacer {
+Sistema . fuera . println  ( "Eligue una opcion" ) ;
+Sistema . fuera . println  ( "Enero" ) ;
+Sistema . fuera . println  ( "Febrero" ) ;
+Sistema . fuera . println  ( "Marzo" ) ;
+Sistema . fuera . println  ( "Abril" ) ;
+Sistema . fuera . println  ( "Mayo" ) ;
+Sistema . fuera . println  ( "Junio" ) ;
+Sistema . fuera . println  ( "Julio" ) ;
+Sistema . fuera . println  ( "Agosto" ) ;
+Sistema . fuera . println  ( "Septiembre" ) ;
+Sistema . fuera . println  ( "Octubre" ) ;
+Sistema . fuera . println  ( "Noviembre" ) ;
+Sistema . fuera . println  ( "Diciembre" ) ;
+Sistema . fuera . println  ( "salir" ) ;
+opcion = Leer . nextInt ( ) ;
+cambiar  ( opcion ) {
+caso 1:
+Sistema . fuera . println  ( "Enero" ) ;
+Sistema . fuera . println  ( "Enero tiene 31 dias" ) ;
+Sistema . fuera . println  ( "Enero tiene 4 fines de semana" ) ;
+pico ;
+
+caso2:
+Sistema . fuera . println  ( "Febrero" ) ;
+Sistema . fuera . println  ( "Febrero tiene 28 días" ) ;
+Sistema . fuera . println  ( "Febrero tiene 4 fines de semana" ) ;
+pico ;
+
+caso3:
+Sistema . fuera . println  ( "Marzo" ) ;
+Sistema . fuera . println  ( "Marzo tiene 31 dias" ) ;
+Sistema . fuera . println  ( "Marzo tiene 5 fines de semana" ) ;
+pico ;
+
+case4:
+Sistema . fuera . println  ( "Abril" ) ;
+Sistema . fuera . println  ( "Abril tiene 30 dias" ) ;
+Sistema . fuera . println  ( "Abril tiene 4 fines de semana" ) ;
+pico ;
+
+case5:
+Sistema . fuera . println  ( "Mayo" ) ;
+Sistema . fuera . println  ( "Mayo tiene 31 días" ) ;
+Sistema . fuera . println  ( "Mayo tiene 4 fines de semana" ) ;
+pico ;
+
+caso6:
+Sistema . fuera . println  ( "Junio" ) ;
+Sistema . fuera . println  ( "Junio ​​tiene 30 dias" ) ;
+Sistema . fuera . println  ( "Junio ​​tiene 5 fines de semana" ) ;
+pico ;
+
+caso7:
+Sistema . fuera . println  ( "Julio" ) ;
+Sistema . fuera . println  ( "Julio tiene 31 dias" ) ;
+Sistema . fuera . println  ( "Julio tiene 4 fines de semana" ) ;
+pico ;
+
+case8:
+Sistema . fuera . println  ( "Agosto" ) ;
+Sistema . fuera . println  ( "Agosto tiene 31 dias" ) ;
+Sistema . fuera . println  ( "Agosto tiene 4 fines de semana" ) ;
+pico ;
+
+case9:
+Sistema . fuera . println  ( "Septiembre" ) ;
+Sistema . fuera . println  ( "Septiembre tiene 30 dias" ) ;
+Sistema . fuera . println  ( "Septiembre tiene 5 fines de semana" ) ;
+pico ;
+
+case10:
+Sistema . fuera . println  ( "Octubre" ) ;
+Sistema . fuera . println  ( "Octubre tiene 31 dias" ) ;
+Sistema . fuera . println  ( "Octubre tiene 4 fines de semana" ) ;
+pico ;
+
+case11:
+Sistema . fuera . println  ( "Noviembre" ) ;
+Sistema . fuera . println  ( "Noviembre tiene 30 dias" ) ;
+Sistema . fuera . println  ( "Noviembre tiene 4 fines de semana" ) ;
+pico ;
+
+case12:
+Sistema . fuera . println  ( "Diciembre" ) ;
+Sistema . fuera . println  ( "Diciembre tiene 31 dias" ) ;
+Sistema . fuera . println  ( "Diciembre tiene 5 fines de semana" ) ;
+pico ;
+
+case13:
+Sistema . fuera . println  ( "Salir" ) ;
+romper ;
+}
+} while ( opcion ! = 13 ) ;
+}
+}
